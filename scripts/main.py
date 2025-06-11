fc754d023d2233025c43d447bda7b58dd6572a50
Geqxon <lars.daams@gmail.com>
2025-06-11 09:37:20 +0200

    well i fucked up

--- scripts/main.py
+++ scripts/main.py
@@ -12,6 +12,7 @@ import json
 import subprocess
 
 subprocess.run(["python", "scripts/home.py"], check=True)
+time.sleep(10)  # Wacht even om zeker te zijn dat de Dobot klaar is
 
 logger = setup_logger()
 
