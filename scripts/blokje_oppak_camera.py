f0eefaa02df524102d466bb4f68744236c477710
Geqxon <lars.daams@gmail.com>
2025-06-12 10:02:30 +0200

    y oppak change

--- scripts/blokje_oppak_camera.py
+++ scripts/blokje_oppak_camera.py
@@ -16,7 +16,7 @@ device = Dobot(port=port)
 # device.clear_alarms()
 print('CURRENT POSE', device.get_pose())
 
-POSITION_A = CustomPosition(x=247, y=-150, z=12, r=80) #oppak
+POSITION_A = CustomPosition(x=247, y=-155, z=12, r=80) #oppak
 POSITION_A_HIGH = CustomPosition(x=247, y=-150, z=80, r=80) #oppak hoog
 POSITION_B = CustomPosition(x=-100, y=-200, z=90, r=-5) #scan locatie
 
