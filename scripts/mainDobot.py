324e4ab3671a9111c5a59d42237e30561fa1bd6b
Geqxon <lars.daams@gmail.com>
2025-06-19 10:08:19 +0200

    coords change v3

--- scripts/mainDobot.py
+++ scripts/mainDobot.py
@@ -13,18 +13,18 @@ def read_errors(device: Dobot):
 def plaats_blokje(device, plek):
     if plek == "plek1":
         # Vul hier de coördinaten in voor plek1
-        device.move_to(x=-2, y=301, z=120, r=175) # Hoog boven de loopband
-        device.move_to(x=-2, y=-301, z=5, r=175)
+        device.move_to(x=-2, y=301, z=120, r=80) # Hoog boven de loopband
+        device.move_to(x=-2, y=-301, z=5, r=80)
         device.grip(False)
         time.sleep(0.5)  # Wacht even om los te laten
-        device.move_to(x=-2, y=-301, z=120, r=175)
+        device.move_to(x=-2, y=-301, z=120, r=80)
     elif plek == "plek2":
         # Vul hier de coördinaten in voor plek2
-        device.move_to(x=320, y=-6, z=120, r=80) # Hoog boven de loopband)
-        device.move_to(x=320, y=-6, z=20, r=80)
+        device.move_to(x=220, y=-6, z=120, r=80) # Hoog boven de loopband)
+        device.move_to(x=220, y=-6, z=20, r=80)
         device.grip(False)
         time.sleep(0.5)  # Wacht even om los te laten
-        device.move_to(x=320, y=-6, z=120, r=80)
+        device.move_to(x=220, y=-6, z=120, r=80)
     else:
         # Onbekend plek
         device.move_to(x=245, y=8, z=120, r=175) # Hoog boven de loopband
