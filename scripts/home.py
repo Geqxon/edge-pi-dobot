5a294cfd0f0c9eb95195e08f6c5991b1699a2de0
Geqxon <lars.daams@gmail.com>
2025-06-19 10:23:47 +0200

    suck false before home

--- scripts/home.py
+++ scripts/home.py
@@ -6,6 +6,7 @@ print(f'available ports: {[x.device for x in available_ports]}')
 port = available_ports[0].device
 device = Dobot(port=port)
 
+device.suck(False)
 device.set_home(x=269, y=10, z=125, r=0)
 device.home()
 
