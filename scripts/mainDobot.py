01d0ea5048ea7c82071db24d564cbc7bd6794c5b
Geqxon <lars.daams@gmail.com>
2025-06-20 10:58:42 +0200

    fucked

--- scripts/mainDobot.py
+++ scripts/mainDobot.py
@@ -21,9 +21,8 @@ def plaats_blokje(device, plek):
         device.move_to(x=-2, y=-201, z=120, r=80)
     elif plek == "plek2":
         # Vul hier de coördinaten in voor plek2
-        device.move_to(x=300, y=3, z=35, r=110) # Hoog boven de loopband)
-        device.move_to(x=305, y=3, z=30, r=130) # Hoog boven de loopband)
-        device.move_to(x=310, y=3, z=20, r=150)
+        device.move_to(x=310, y=3, z=30, r=110) # Hoog boven de loopband)
+        device.move_to(x=310, y=3, z=20, r=120)
         device.grip(False)
         time.sleep(0.5)  # Wacht even om los te laten
         device.suck(False)
