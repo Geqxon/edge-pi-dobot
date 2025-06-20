aed992492dc9b1f3560d959a540b05eba431cb4f
Geqxon <lars.daams@gmail.com>
2025-06-20 10:34:38 +0200

    coords change idk anymore

--- scripts/mainDobot.py
+++ scripts/mainDobot.py
@@ -21,21 +21,20 @@ def plaats_blokje(device, plek):
         device.move_to(x=-2, y=-201, z=120, r=80)
     elif plek == "plek2":
         # Vul hier de coördinaten in voor plek2
-        device.move_to(x=280, y=3, z=120, r=110) # Hoog boven de loopband)
-        device.move_to(x=280, y=3, z=20, r=110)
+        device.move_to(x=320, y=3, z=120, r=110) # Hoog boven de loopband)
+        device.move_to(x=320, y=3, z=20, r=110)
         device.grip(False)
         time.sleep(0.5)  # Wacht even om los te laten
         device.suck(False)
-        device.move_to(x=280, y=3, z=120, r=110)
+        device.move_to(x=320, y=3, z=30, r=110)
     else:
         # Onbekend plek
-        device.move_to(x=280, y=3, z=120, r=110) # Hoog boven de loopband)
-        device.move_to(x=280, y=3, z=20, r=110)
+        device.move_to(x=320, y=3, z=120, r=110) # Hoog boven de loopband)
+        device.move_to(x=320, y=3, z=20, r=110)
         device.grip(False)
         time.sleep(0.5)  # Wacht even om los te laten
         device.suck(False)
-        device.move_to(x=280, y=3, z=120, r=110)
-
+        device.move_to(x=320, y=3, z=30, r=110)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--plaats", type=str, required=True, help="Waar moet het blokje geplaatst worden?")
