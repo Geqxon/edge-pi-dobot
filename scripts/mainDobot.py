adf098f1c3d70db932b4d406f41631f4aad6689e
Geqxon <lars.daams@gmail.com>
2025-06-23 11:09:06 +0200

    logo andere kant op kijkend

--- scripts/mainDobot.py
+++ scripts/mainDobot.py
@@ -13,21 +13,21 @@ def read_errors(device: Dobot):
 def plaats_blokje(device, plek):
     if plek == "plek1":
         # Vul hier de coördinaten in voor plek1
-        device.move_to(x=75, y=175, z=77, r=85) #voor de mgazijn van zwart blokje met grijs logo
-        device.move_to(x=75, y=250, z=77, r=85) #druk blokjes verder magazijn in
-        device.move_to(x=75, y=250, z=70, r=85) #laat blokje vallen
+        device.move_to(x=75, y=175, z=77, r=171) #voor de mgazijn van zwart blokje met grijs logo
+        device.move_to(x=75, y=250, z=77, r=171) #druk blokjes verder magazijn in
+        device.move_to(x=75, y=250, z=70, r=171) #laat blokje vallen
         device.grip(False)
         time.sleep(0.5)  # Wacht even om los te laten
         device.suck(False)
-        device.move_to(x=75, y=250, z=95, r=85) #grijper boven blokje brengen voor volgende stap.
+        device.move_to(x=75, y=250, z=95, r=171) #grijper boven blokje brengen voor volgende stap.
     elif plek == "plek2":   
-        device.move_to(x=135, y=180, z=77, r=85) #voor de mgazijn van zwart blokje met grijs logo
-        device.move_to(x=135, y=250, z=77, r=85) #druk blokjes verder magazijn in
-        device.move_to(x=135, y=250, z=70, r=85) #laat blokje vallen
+        device.move_to(x=135, y=180, z=77, r=-6) #voor de mgazijn van zwart blokje met grijs logo
+        device.move_to(x=135, y=250, z=77, r=-6) #druk blokjes verder magazijn in
+        device.move_to(x=135, y=250, z=70, r=-6) #laat blokje vallen
         device.grip(False)
         time.sleep(0.5)  # Wacht even om los te laten
         device.suck(False)
-        device.move_to(x=135, y=250, z=95, r=85) #grijper boven blokje brengen voor volgende stap.
+        device.move_to(x=135, y=250, z=95, r=-6) #grijper boven blokje brengen voor volgende stap.
     else:
         # Onbekend plek
         device.move_to(x=250, y=3, z=50, r=130) # Hoog boven de loopband
