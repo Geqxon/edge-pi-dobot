2a16641767f281ad66ccbbc48897e41c031d94d0
Geqxon <lars.daams@gmail.com>
2025-06-26 10:47:36 +0200

    small coords change on location

--- scripts/blokje_oppak_camera.py
+++ scripts/blokje_oppak_camera.py
@@ -16,8 +16,8 @@ device = Dobot(port=port)
 # device.clear_alarms()
 print('CURRENT POSE', device.get_pose())
 
-POSITION_A = CustomPosition(x=247, y=-145, z=8, r=80) #oppak
-POSITION_A_HIGH = CustomPosition(x=247, y=-145, z=100, r=80) #oppak hoog
+POSITION_A = CustomPosition(x=247, y=-150, z=8, r=80) #oppak
+POSITION_A_HIGH = CustomPosition(x=247, y=-150, z=100, r=80) #oppak hoog
 POSITION_B = CustomPosition(x=98, y=-235, z=115, r=12) #scan locatie
 
 device.move_to(position=POSITION_A_HIGH)
